be423cdcaa972dc6db117700894ab37268ab1a94
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-05-26 15:56:06 +0530

    editting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,14 @@
-# from pyvirtualdisplay import Display
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManage
 def oneTimeSetUp():
-        opts = webdriver.ChromeOptions()
-        opts.add_argument('--no-sandbox')
-        opts.add_argument('--disable-dev-shm-usage')
-        opts.add_argument('--ignore-certificate-errors')
-    #options.add_argument("--test-type")
-        opts.add_argument("--headless")
-        opts.add_argument("--incognito")
 #         os.chmod('./chromedriver', 755)
 
 #         display = Display(visible=1)
 #         display.start()
         baseURL = "https://letskodeit.teachable.com/"
         print("before webdriver")
-        driver = webdriver.Chrome(options=opts,ChromeDriverManager().install())
+        driver = webdriver.Chrome(ChromeDriverManager().install())
         print("after webdriver")
-        
-        driver = webdriver.Chrome(executable_path='./chromedriver')
         driver.get(baseURL)
         driver.maximize_window()
         print("Running tests on chrome")
