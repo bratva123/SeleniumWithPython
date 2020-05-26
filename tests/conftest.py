8b633d0876b3e8a1144a7583a93896608d782f4d
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-05-26 09:09:19 +0530

    current directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ def oneTimeSetUp():
 #         display = Display(visible=1)
 #         display.start()
         baseURL = "https://letskodeit.teachable.com/"
-        driver = webdriver.Chrome(executable_path=r"/home/global/Downloads/chromedriver")
+        driver = webdriver.Chrome(executable_path='./chromedriver')
         driver.get(baseURL)
         driver.maximize_window()
         print("Running tests on chrome")
