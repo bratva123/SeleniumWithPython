d69166f24fd5204158308be9a692469ed844b65b
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-05-26 19:32:57 +0530

    adding firefox browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,19 +2,21 @@ from selenium import webdriver
 from pyvirtualdisplay import Display
 
 def oneTimeSetUp():
-        chrome_options = webdriver.ChromeOptions()
-#         Display(visible=1, size=(1000, 1000)).start()
-        chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
+#         chrome_options = webdriver.ChromeOptions()
+# #         Display(visible=1, size=(1000, 1000)).start()
+#         chrome_options.add_argument('--disable-extensions')
+#         chrome_options.add_argument('--disable-dev-shm-usage')
+#         chrome_options.add_argument('--headless')
+#         chrome_options.add_argument('--no-sandbox')
+        option = webdriver.FirefoxOptions()
+        option.add_argument('--headless')
+        option.add_argument('--disable-dev-shm-usage')
+        option.add_argument('window-size=1920x1080')
+        # driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='./chromedriver')
+        driver = webdriver.Firefox(executable_path='./geckodriver',options=option)
         baseURL = "https://letskodeit.teachable.com/"
-        print("before webdriver")
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        print("after webdriver")
         driver.get(baseURL)
-        driver.maximize_window()
-        print("Running tests on chrome")
+        print("Running tests on firefox")
         return driver
 
         # yield driver
