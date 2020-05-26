eaf089f8e56089569a51331e238116d7b908e659
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-05-26 16:06:48 +0530

    no sandbox

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ def oneTimeSetUp():
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--disable-extensions')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
         baseURL = "https://letskodeit.teachable.com/"
         print("before webdriver")
         driver = webdriver.Chrome(chrome_options=chrome_options)
