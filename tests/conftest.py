c81a5e2d6dd0d1e7ba4ecaa3b1a4223f9de069d5
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-05-26 17:03:57 +0530

    commenting display

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-from pyvirtualdisplay import Display
+# from pyvirtualdisplay import Display
 
 def oneTimeSetUp():
         chrome_options = webdriver.ChromeOptions()
-        Display(visible=1, size=(1000, 1000)).start()
+#         Display(visible=1, size=(1000, 1000)).start()
         chrome_options.add_argument('--disable-extensions')
         chrome_options.add_argument('--disable-dev-shm-usage')
 #         chrome_options.add_argument('--headless')
