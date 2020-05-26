3121d5f85fdf43b4461b8098967bac4b1a7a9654
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-05-26 19:41:42 +0530

    adding geckodriver to local bin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
+import os
 
 def oneTimeSetUp():
 #         chrome_options = webdriver.ChromeOptions()
@@ -8,12 +9,13 @@ def oneTimeSetUp():
 #         chrome_options.add_argument('--disable-dev-shm-usage')
 #         chrome_options.add_argument('--headless')
 #         chrome_options.add_argument('--no-sandbox')
+        
         option = webdriver.FirefoxOptions()
         option.add_argument('--headless')
         option.add_argument('--disable-dev-shm-usage')
         option.add_argument('window-size=1920x1080')
         # driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='./chromedriver')
-        driver = webdriver.Firefox(executable_path='./geckodriver',options=option)
+        driver = webdriver.Firefox(options=option)
         baseURL = "https://letskodeit.teachable.com/"
         driver.get(baseURL)
         print("Running tests on firefox")
